19134dcdc6959708d6ec1fb9de4737c0d0f24103
Maribelll <48179911+Maribelll@users.noreply.github.com>
2021-11-15 09:27:03 +0300

    Update Product.js

--- components/Product.js
+++ components/Product.js
@@ -116,14 +116,9 @@ export default function Product() {
         <div className="product">
           <div className="image-product">
             <Image src={profilePic} alt="img" />
-            <img
-              className="planet-image"
-              src={`https://imgproxy.by.dev.family/unxn1YtzK0U17tHAeHEu7a5LB5AQ6BMc-WMjYM2Wd9I/w:266/h:266/czM6Ly9nZXRsZW5zLzExNi8xM2hxLndlYnA.jpg`}
-              alt=""
-            />
           </div>
           <div className="description-product">
-            <p className="title-product">{}</p>
+            <p className="title-product">Olympus OM-D E-M5</p>
             <span className="price-product">28 000 &#8381;</span>
             <input
               className="button-product"
@@ -140,44 +135,3 @@ export default function Product() {
     </>
   );
 }
-
-export const getStaticProps = async (context) => {
-  const res = await fetch(
-    "https://getlens-master.stage.dev.family/api/pages/obektivy?page=1"
-  );
-  const todos = await res.json();
-
-  return {
-    props: {
-      todos,
-    },
-  };
-};
-
-class ApiService {
-  _apiBase =
-    "https://getlens-master.stage.dev.family/api/pages/obektivy?page=1";
-  async getResourse(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch" ${url}` + `, received ${res.status}`);
-    }
-
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResourse(`/title/`);
-    return res.results;
-  }
-
-  getTitle(id) {
-    return this.getResourse(`/title/${id}`);
-  }
-}
-const swapi = new ApiService();
-
-swapi.getTitle(1391).then((p) => {
-  console.log(p.title);
-});
